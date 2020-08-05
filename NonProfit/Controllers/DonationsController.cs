88370ad79a9a430fbe7e6fc83b3ee226fd6211a2
Cody Fritz <clanalia55@gmail.com>
2020-08-05 11:41:26 -0700

    add initial files and setup
    Co-authored-by: BAPotts <pottsbeverly@gmail.com>

--- NonProfit/Controllers/DonationsController.cs
+++ NonProfit/Controllers/DonationsController.cs
@@ -1,74 +1,78 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using NonProfit.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NonProfit.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace NonProfit.Controllers
-// {
-//   public class DonationsController : Controller
-//   {
-//     private readonly NonProfitContext _db;
-//     public DonationsController(NonProfitContext db)
-//     {
-//       _db = db;
-//     }
-//     ////////
-//     public ActionResult Details( int id )
-//     {
-//       Donation thisDonation = _db.Donations.Include(Donation => Donation.Donor).FirstOrDefault(donations => donations.DonationId == id);
-//       return View(thisDonation);
-//     }
-//     ////////
-//     public ActionResult Create()
-//     {
-//     ViewBag.DonorId = new SelectList(_db.Donors, "DonorId", "Name");
-//     return View();
-//     }
-//     ////////
-//     public ActionResult Edit (int id)
-//     {
-//       var thisDonation = _db.Donations.FirstOrDefault(Donations => Donations.DonationId == id);
-//       ViewBag.DonorId = new SelectList(_db.Donors, "DonorId", "Name");
-//       return View(thisDonation);
-//     }
-//     ////////
-//     public ActionResult Delete(int id)
-//     {
-//       var thisDonation = _db.Donations.FirstOrDefault(donations => donations.DonationId == id);
-//       return View(thisDonation);
-//     }
-//     ////////
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       var thisDonation = _db.Donations.FirstOrDefault(donations => donations.DonationId == id);
-//       _db.Donations.Remove(thisDonation);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//     ////////
-//     [HttpPost]
-//     public ActionResult Edit(Donation donation)
-//     {
-//       _db.Entry(donation).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//     ////////
-//     [HttpPost]
-//     public ActionResult Create(Donation donation)
-//     {
-//       _db.Donations.Add(donation);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//     ////////
-//     public ActionResult Index()
-//     {
-//       List<Donation> model = _db.Donations.Include(donations => donations.Donor).ToList();
-//       return View(model);
-//     }
-//   }
-// }
+namespace NonProfit.Controllers
+{
+  public class DonationsController : Controller
+  {
+    private readonly NonProfitContext _db;
+    public DonationsController(NonProfitContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+
+      return View(_db.Donations.ToList());
+    }
+  
+    public ActionResult Details( int id )
+    {
+      var thisDonation = _db.Donations.Include(donation => donation.Donors).ThenInclude(join =>join.Donor).FirstOrDefault(donation => donation.DonationId == id);
+      return View(thisDonation);
+    }
+    ////////
+    public ActionResult Create()
+    {
+    ViewBag.DonorId = new SelectList(_db.Donors, "DonorId", "Name");
+    return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Donation donation, int DonorId)
+    {
+      _db.Donations.Add(donation);
+      if (DonorId !=0)
+      {
+        _db.DonorDonation.Add(new DonorDonation() {DonorId = DonorId, DonationId = donation.DonationId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    // ////////
+    // public ActionResult Edit (int id)
+    // {
+    //   var thisDonation = _db.Donations.FirstOrDefault(Donations => Donations.DonationId == id);
+    //   ViewBag.DonorId = new SelectList(_db.Donors, "DonorId", "Name");
+    //   return View(thisDonation);
+    // }
+    // ////////
+    // public ActionResult Delete(int id)
+    // {
+    //   var thisDonation = _db.Donations.FirstOrDefault(donations => donations.DonationId == id);
+    //   return View(thisDonation);
+    // }
+    // ////////
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisDonation = _db.Donations.FirstOrDefault(donations => donations.DonationId == id);
+    //   _db.Donations.Remove(thisDonation);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+    // ////////
+    // [HttpPost]
+    // public ActionResult Edit(Donation donation)
+    // {
+    //   _db.Entry(donation).State = EntityState.Modified;
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+    // ////////
+    
+  }
+}
