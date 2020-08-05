610e4cafc3bcba15f92c99058fa90e034f2d68a3
Cody Fritz <clanalia55@gmail.com>
2020-08-05 09:49:38 -0700

    add initial files and setup
    Co-authored-by: BAPotts <pottsbeverly@gmail.com>

---
+++ NonProfit/Models/NonProfitContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace NonProfit.Models
+{
+  public class NonProfitContext : DbContext
+  {
+    public virtual DbSet<Donor> Donors { get; set; }
+    public DbSet<Donation> Donations { get; set; }
+
+    public NonProfitContext(DbContextOptions options) : base(options) { }
+  }
+}
